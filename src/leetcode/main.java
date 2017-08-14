d797f0ce5f0e03347fa7b1d8afbfbd7c9ed2f8a6
akimble <kimble00550117@gmail.com>
2017-08-14 18:08:45 -0400

    Exercises from today and yesterday

---
+++ src/leetcode/main.java
@@ -0,0 +1,198 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+	public static void main(String[] args) {
+//		String moves = "LDRRLRUULR";
+//		boolean answer = judgeCircle(moves);
+//		System.out.println(answer);
+		
+//		int x = 1;
+//		int y = 4;
+//		int answer = hammingDistance(x, y);
+//		System.out.println(answer);
+		
+//		TreeNode t1 = new TreeNode(1);
+//		TreeNode t1Left = new TreeNode(3);
+//		t1.left = t1Left;
+//		t1.right = new TreeNode(2);
+//		t1Left.left = new TreeNode(5);
+//		TreeNode t2 = new TreeNode(2);
+//		TreeNode t2Left = new TreeNode(1);
+//		TreeNode t2Right = new TreeNode(3);
+//		t2Left.right = new TreeNode(4);
+//		t2Right.right = new TreeNode(7);
+//		TreeNode answer = mergeTrees(t1, t2);
+//		System.out.println(answer.val+", "+answer.left.val+", "+answer.right.val);
+		
+//		String s = "hello";
+//		String answer = reverseString(s);
+//		System.out.println(answer);
+		
+//		List<String> answer = fizzBuzz(15);
+//		System.out.println(answer);
+		
+		int[] nums = new int[] {1,2,2,4,1,4,5};
+		int answer = singelNumber(nums);
+		System.out.println(answer);
+	}
+	
+	// Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+	// The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). 
+	// The output should be true or false representing whether the robot makes a circle. 
+	public static boolean judgeCircle(String moves) {
+	  char[] charArray = moves.toCharArray();
+	  int up = 0;
+	  int left = 0;
+	  
+	  for (int i=0;i<moves.length();++i) {
+		  switch(charArray[i]) {
+		  	case 'U' :
+	    		++up;
+	    		break;
+	    	case 'D' :
+	    		--up;
+	    		break;
+	    	case 'L' :
+	    		++left;
+	    		break;
+	    	case 'R' :
+	    		--left;
+	    		break;
+	    }
+	  }
+	  
+	  if (up == 0 && left == 0)
+		  return true;
+	  else
+		  return false;
+	}
+	
+	// The Hamming distance between two integers is the number of positions
+	// at which the corresponding bits are different.Given two integers x and y, 
+	// calculate the Hamming distance. Note:0 ≤ x, y < 231. 
+	public static int hammingDistance(int x, int y) {
+		int diff = 0;
+		final String zero = "0";
+		String binX = Integer.toBinaryString(x);
+		String binY = Integer.toBinaryString(y);
+		
+		if (binX.length() < binY.length()) {
+			for (int i=binX.length();i<binY.length();++i) {
+				binX = zero + binX;
+			}
+			diff = findDiff(binX, binY);
+		}
+		else {
+			for (int i=binY.length();i<binX.length();++i) {
+				binY += "0";
+			}
+			diff = findDiff(binX, binY);
+		}
+		
+		return diff;
+	}
+	public static int findDiff(String binX, String binY) {
+		char[] binXArray = binX.toCharArray();
+		char[] binYArray = binY.toCharArray();
+		int diff = 0;
+		
+		for (int i=0;i<binXArray.length;++i) {
+			if (binXArray[i] != binYArray[i])
+				++diff;
+		}
+		
+		return diff;
+	}
+	
+	// Looked at one of the answers bc I didn't fully understand the class structure. This
+	// is pretty much word-for-word so I don't know if this question counts for me but I
+	// did learn how to do it so I don't know.
+	// Given two binary trees and imagine that when you put one of them to cover the
+	// other, some nodes of the two trees are overlapped while the others are not.You need
+	// to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+	// then sum node values up as the new value of the merged node. Otherwise, the 
+	// NOT null node will be used as the node of new tree. 
+	public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+		int t1Andt2Sum = 0;
+		
+		if (t1 == null && t2 == null)
+			return null;
+		
+		t1Andt2Sum = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+		TreeNode newNode = new TreeNode(t1Andt2Sum);
+		
+		newNode.left = mergeTrees((t1 == null ? null : t1.left), (t2 == null ? null : t2.left));
+		newNode.right = mergeTrees((t1 == null ? null : t1.right), (t2 == null ? null : t2.right));
+		
+		return newNode;
+	}
+	
+	// Write a function that takes a string as input and returns the string reversed.
+	// Was too slow on last case so I commented out and used StringBuilder(duh).
+	public static String reverseString(String s) {
+//		String reversedS = "";
+//		char[] sArray = s.toCharArray();
+//		
+//		for (int i=0;i<sArray.length;++i) {
+//			reversedS += sArray[(sArray.length-1)-i];
+//		}
+//		
+//		return reversedS;
+		return new StringBuilder(s).reverse().toString();
+	}
+	
+	// Write a program that outputs the string representation of numbers from 1 to n.
+	// But for multiples of three it should output “Fizz” instead of the number and for
+	// the multiples of five output “Buzz”. For numbers which are multiples of both three
+	// and five output “FizzBuzz”.
+	public static List<String> fizzBuzz(int n) {
+		List<String> answer = new ArrayList<String>(n);
+		
+		for (int i=1;i<n+1;++i) {
+			if (i%3 == 0 && i%5 == 0) {
+				answer.add("FizzBuzz");
+			}
+			else if (i%3 == 0) {
+				answer.add("Fizz");
+			}
+			else if (i%5 == 0) {
+				answer.add("Buzz");
+			}
+			else {
+				answer.add(String.valueOf(i));
+			}
+		}
+		
+		return answer;
+	}
+	
+	// Given an array of integers, every element appears twice except for one. 
+	// Find that single one. Note:Your algorithm should have a linear runtime complexity.
+	// Could you implement it without using extra memory?
+	// TOO LONG. Commented out in exchange for bitwise XOR answer I found (ughhh).
+	public static int singelNumber(int[] nums) {
+//		List<Integer> answer = new ArrayList<Integer>(0);
+//		
+//		for (int i=0;i<nums.length;++i) {
+//			if (answer.contains(nums[i]))
+//				answer.remove(new Integer(nums[i]));
+//			else
+//				answer.add(nums[i]);
+//		}
+//		
+//		return answer.get(0);
+		int ans = 0;
+		
+		for (int i=0;i<nums.length;++i) {
+			// 0 = FALSE, N = a number, and XOR is commutative so XOR: 0 ^ N = N, N ^ N = 0
+			// Array: {2,1,4,5,2,4,1}, XOR: ((2^2)^(1^1)^(4^4)^(5)) => (0^0^0^5) => 5
+			// Commutative means it doesn't matter where the numbers in the array are
+			ans = ans ^ nums[i];
+		}
+		
+		return ans;
+	}
+}
